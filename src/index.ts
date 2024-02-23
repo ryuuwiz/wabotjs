577b045de5a06caf8a4d45e5cdd65ee4023842f1
ryuuwiz <ryuuwiz@gmail.com>
2024-02-23 18:04:34 +0700

    feat: self sticker message

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
 import * as qrcode from "qrcode-terminal"
-import { Client } from "whatsapp-web.js"
+import { Client, LocalAuth, MessageContent } from "whatsapp-web.js"
 import { nanoid } from 'nanoid'
 
-const client = new Client({});
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 })
+
 client.on('ready', () => {
-  console.log('Client is ready!');
+  console.log(`
+  Whatsapp bot - Ryuuwiz
+  Feat:
+    - !hello -> Hello Message
+    - !s -> Generate Sticker from Image
+`);
 });
 
-// Hello Message
-client.on('message', async (message) => {
-  if (message.body === '!hello') {
-    await message.reply('Hello, World!');
-  }
-});
-
-// Sticker Generator
 client.on("message", async (msg) => {
+  if (msg.body === '!menu') {
+    await msg.reply(`
+      Whatsapp bot - Ryuuwiz
+      Feat:
+        - !hello -> Hello Message
+        - !s -> Generate Sticker from Image
+    `);
+    console.log(msg)
+  }
+  if (msg.body === '!hello') {
+    await msg.reply('Hello, World!');
+    console.log(msg)
+  }
   if (msg.body.startsWith("!s") && msg.type === 'image') {
-    let media;
+    let media: MessageContent;
     try {
       media = await msg.downloadMedia();
     } catch (error) {
@@ -34,6 +47,26 @@ client.on("message", async (msg) => {
       stickerAuthor: "John Doe",
       stickerName: nanoid()
     })
+    console.log(msg)
+  }
+})
+
+client.on("message_create", async (msg) => {
+  if (msg.body.startsWith("!s") && msg.type === 'image') {
+    let media: MessageContent;
+    try {
+      media = await msg.downloadMedia();
+    } catch (error) {
+      console.log(error);
+      return msg.reply("Download image failed!");
+    }
+
+    client.sendMessage(msg.from, media, {
+      sendMediaAsSticker: true,
+      stickerAuthor: "John Doe",
+      stickerName: nanoid()
+    })
+    console.log(msg)
   }
 })
 
