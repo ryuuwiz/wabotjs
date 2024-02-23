ad8a280f39a4cf814a634982b9ae0cffd0386b1e
ryuuwiz <ryuuwiz@gmail.com>
2024-02-23 17:01:04 +0700

    feat: sticker gen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("hello")
+import * as qrcode from "qrcode-terminal"
+import { Client } from "whatsapp-web.js"
+import { nanoid } from 'nanoid'
+
+const client = new Client({});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+})
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+// Hello Message
+client.on('message', async (message) => {
+  if (message.body === '!hello') {
+    await message.reply('Hello, World!');
+  }
+});
+
+// Sticker Generator
+client.on("message", async (msg) => {
+  if (msg.body.startsWith("!s") && msg.type === 'image') {
+    let media;
+    try {
+      media = await msg.downloadMedia();
+    } catch (error) {
+      console.log(error);
+      return msg.reply("Download image failed!");
+    }
+
+    client.sendMessage(msg.from, media, {
+      sendMediaAsSticker: true,
+      stickerAuthor: "John Doe",
+      stickerName: nanoid()
+    })
+  }
+})
+
+client.initialize();
+
